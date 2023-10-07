b59a0db0578b7e7c8da0a6bb05306f82acf48d60
aluno <aluno@S2C59E5BE94E5>
2023-10-07 10:13:11 -0300

    Adicionar arquivos de projeto.

---
+++ Atividade0710/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Data.SqlClient;
+
+namespace Atividade0710
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+
+        public void connecting()
+        {
+            String ConnectionString = @"Data Source=(LocalDB)\MSSQLLocalDB; 
+                                AttachDbFilename=|DataDirectory|\APP_DATA\Database.mdf; 
+                                Integrated Security = True";
+            using (SqlConnection connection = new SqlConnection(ConnectionString))
+            {
+                connection.Open();
+                System.Console.WriteLine("Conexão estabelecida com sucesso");
+            }
+        }
+}
+
+           // public void testaConexao()
+            //{
+              //  try
+               // {
+              //      String conString = @"Data Source=(LocalDB)\MSSQLLocalDB; 
+              //                  AttachDbFilename=|DataDirectory|\APP_DATA\Database.mdf; 
+              //                  Integrated Security = True";
+              //      SqlConnection connection = new SqlConnection(conString);
+               //     connection.Open();
+               //     Console.WriteLine("\nConexão aberta com sucesso!\n");
+              //  }
+              //  catch (SqlException ex)
+               // {
+              //      Console.WriteLine($"\nOcorreu uma exceção : {ex.Message}\n");
+              //  }
+            //}
+
+
+
+        }
+}
